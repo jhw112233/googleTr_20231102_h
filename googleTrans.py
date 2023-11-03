37977666ea59967bd4568f68483928dbfd49e36e
jhw112233 <jhw12150@naver.com>
2023-11-03 11:15:19 +0900

    ver

--- googleTrans.py
+++ googleTrans.py
@@ -6,7 +6,7 @@ from PyQt5.QtGui import *
 
 import googletrans #------------- 모든 모듈 준비 ---------
 
-form_class = uic.loadUi('ui/gooUi.ui')[0]
+form_class = uic.loadUiType('ui/gooUi.ui')[0]
 #qt 디자이너에서 제작한 ui 불러오기
 
 class MyGoogleTrans(QMainWindow, form_class):#두개 상속받음
@@ -17,11 +17,33 @@ class MyGoogleTrans(QMainWindow, form_class):#두개 상속받음
         self.setWindowIcon(QIcon('icon/goo1.png'))#번역기 앱 아이콘
         self.statusBar().showMessage('Google Trans App v1.0 copyright ⓒ W. conpany ')#상태표시줄
 
-if __name__=='__main__':#다른곳에서 불러도 실행 안되도록 함
-    app= QApplication(sys.argv)
-    myApp=MyGoogleTrans()
-    myApp.show()
-    sys.exit(app.exec_())
+        self.btn_trans.clicked.connect(self.trans_function)  # 시그널
+        self.btn_clr.clicked.connect(self.reset_kor)
+
+    def trans_function(self):  # 슬롯
+        trans_kor = self.leEdit.text()
+
+        trans = googletrans.Translator()
+        trans_eng = trans.translate(trans_kor, dest='en')
+        trans_jap = trans.translate(trans_kor, dest='ja')
+        trans_chn = trans.translate(trans_kor, dest='zh-cn')
+
+
+
+        self.txEdit1.append(trans_eng.text)
+        self.txEdit2.append(trans_jap.text)
+        self.txEdit3.append(trans_chn.text)
+
+    def reset_kor(self):
+        self.leEdit.clear()
+
+
+
+if __name__ == '__main__':#다른곳 불러도 실행 안되도록
+    app1 = QApplication(sys.argv)
+    myApp1 = MyGoogleTrans()
+    myApp1.show()
+    sys.exit(app1.exec_())
 
     
 
